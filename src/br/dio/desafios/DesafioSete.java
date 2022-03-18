74bf71ab6c97ab8516efd937d75dedf8940d05a0
Sadraque Bezerra <87199938+SadraqueBezerra@users.noreply.github.com>
2022-03-18 09:29:20 -0300

    Update DesafioSete.java

--- src/br/dio/desafios/DesafioSete.java
+++ src/br/dio/desafios/DesafioSete.java
@@ -1,3 +1,17 @@
+/*Desafio
+Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a 
+média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que 
+cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+Entrada
+O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+
+Saída
+Calcule e imprima a variável MEDIA conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco 
+antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de 
+imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+*/
+
 package br.dio.desafios;
 
 import java.util.Scanner;
@@ -11,10 +25,10 @@ public class DesafioSete {
         A = sc.nextDouble();
         B = sc.nextDouble();
 
-        //TODO: Complete os espa�os em branco com as respectivas vari�veis para o c�lculo da m�dia.
+        //TODO: Complete os espaços em branco com as respectivas variáveis para o cálculo da média.
         media = (A * 3.5 + B * 7.5)/11; 
 
-        //TODO: Complete com a vari�vel que representa o resultado da m�dia.
+        //TODO: Complete com a variável que representa o resultado da média.
         System.out.printf("MEDIA = %.5f",  media);
         System.out.println();
     }
