577146f3438dbb6ee97960c1f60bcea142849745
Sadraque Bezerra <87199938+SadraqueBezerra@users.noreply.github.com>
2022-03-18 09:26:07 -0300

    Update DesafioQuatro.java

--- src/br/dio/desafios/DesafioQuatro.java
+++ src/br/dio/desafios/DesafioQuatro.java
@@ -1,3 +1,16 @@
+/*Desafio
+Paulinho tem em suas mãos um pequeno problema. A professora lhe pediu que ele construísse um programa para verificar, 
+à partir de dois valores inteiros A e B, se B corresponde aos últimos dígitos de A.
+
+Entrada
+A entrada consiste de vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade 
+de casos de teste. Cada caso de teste consiste de dois inteiros A (1 ≤ A < 231 ) e B (1 ≤ B < 231) positivos.
+
+Saída
+Para cada caso de entrada imprima uma mensagem indicando se o segundo valor encaixa no primeiro valor, confome exemplo 
+abaixo.
+*/
+
 package br.dio.desafios;
 
 import java.util.Scanner;
@@ -24,6 +37,5 @@ public class DesafioQuatro {
 
 			}
 		}
-
 	}
 }
