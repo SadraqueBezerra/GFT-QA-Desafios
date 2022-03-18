9d0bd74c0d6c90357e70c230d00cfd799825fa30
Sadraque Bezerra <87199938+SadraqueBezerra@users.noreply.github.com>
2022-03-18 09:26:53 -0300

    Update DesafioCinco.java

--- src/br/dio/desafios/DesafioCinco.java
+++ src/br/dio/desafios/DesafioCinco.java
@@ -1,3 +1,15 @@
+/*Desafio
+
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", 
+indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+*/
+
 package br.dio.desafios;
 
 import java.io.IOException;
