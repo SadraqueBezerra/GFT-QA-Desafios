62c06b780be00e960e2f3b49bd5519973b643e05
Sadraque Bezerra <87199938+SadraqueBezerra@users.noreply.github.com>
2022-03-18 09:21:18 -0300

    Update Desafio1.java

--- src/br/dio/desafios/Desafio1.java
+++ src/br/dio/desafios/Desafio1.java
@@ -5,6 +5,13 @@ Em uma hora (60 minutos) a moto Y consegue se distanciar 30 quilômetros da moto
 quilômetro a cada 2 minutos.
 O seu desafio é ler a distância (em Km) e calcular quanto tempo leva (em minutos) para a moto Y tomar essa distância da 
 outra moto.
+
+Entrada
+O arquivo de entrada contém um número inteiro K que representa a quantidade de quilômetro que que a moto Y deve estar da 
+moto X.
+
+Saída
+Imprima o tempo necessário para a moto Y ficar com a quantidade K de quilômetro da moto X, seguido da mensagem " minutos".
 */
 
 package br.dio.desafios;
